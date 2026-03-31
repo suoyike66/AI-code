7c0860cf22f1027dee169cda57eff09cc2716eed
suoyike66 <3590646448@qq.com>
2026-03-31 15:55:39 +0800

    用户模块后端： 1.开发MyBatis Felx 代码生成器 2.开发核心功能，比如用户注册、用户登录功能、获取登录用户信息、用户注销等 3.全局权限校验AOP 4.开发管理员增删改查接口 5.自定义配置模块解决丢失精度问题
    Signed-off-by: suoyike66 <3590646448@qq.com>

--- ai-code-springboot/src/main/java/com/suoyike/aicodespringboot/AiCodeSpringbootApplication.java
+++ ai-code-springboot/src/main/java/com/suoyike/aicodespringboot/AiCodeSpringbootApplication.java
@@ -1,9 +1,11 @@
 package com.suoyike.aicodespringboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.suoyike.aicodespringboot.mapper")
 public class AiCodeSpringbootApplication {
 
     public static void main(String[] args) {
