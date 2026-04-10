a6b4c851f12ec4e62b899fd64b106cd56a4b2fe0
suoyike66 <3590646448@qq.com>
2026-04-10 17:18:49 +0800

    系统优化（实时性优化）
    1.Vue 工程异步构建改为同步构建，构建完成后才结束SSE连接
    
    Signed-off-by: suoyike66 <3590646448@qq.com>

--- ai-code-springboot/src/main/java/com/suoyike/aicodespringboot/core/handler/JsonMessageStreamHandler.java
+++ ai-code-springboot/src/main/java/com/suoyike/aicodespringboot/core/handler/JsonMessageStreamHandler.java
@@ -1,13 +1,11 @@
 package com.suoyike.aicodespringboot.core.handler;
 
-import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.suoyike.aicodespringboot.ai.model.message.*;
 import com.suoyike.aicodespringboot.ai.tools.BaseTool;
 import com.suoyike.aicodespringboot.ai.tools.ToolManager;
-import com.suoyike.aicodespringboot.constant.AppConstant;
 import com.suoyike.aicodespringboot.core.builder.VueProjectBuilder;
 import com.suoyike.aicodespringboot.model.entiey.User;
 import com.suoyike.aicodespringboot.model.enums.ChatHistoryMessageTypeEnum;
@@ -27,9 +25,6 @@ import java.util.Set;
 @Slf4j
 @Component
 public class JsonMessageStreamHandler {
-
-    @Resource
-    private VueProjectBuilder vueProjectBuilder;
 
     @Resource
     private ToolManager toolManager;
@@ -61,10 +56,7 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    // 异步构造 Vue 项目
-                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
-                    vueProjectBuilder.buildProjectAsync(projectPath);
-                })
+                    })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息
                     String errorMessage = "AI回复失败: " + error.getMessage();
