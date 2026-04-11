65e51099c9ff96d523ff55b1c40d9bcbbef68946
suoyike66 <3590646448@qq.com>
2026-04-11 18:20:16 +0800

    微服务改造 - 数据模型模块

---
+++ ai-code-microservice/ai-code-model/src/main/java/com/suoyike/aicodemodel/model/dto/app/AppAddRequest.java
@@ -0,0 +1,19 @@
+package com.suoyike.aicodemodel.model.dto.app;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 创建应用请求
+*/
+@Data
+public class AppAddRequest implements Serializable {
+
+    /**
+     * 应用初始化的 prompt
+     */
+    private String initPrompt;
+
+    private static final long serialVersionUID = 1L;
+}
