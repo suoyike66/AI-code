ba3bd55a4ef78b46dcdf615e503084e97b48563d
suoyike66 <3590646448@qq.com>
2026-04-11 18:32:58 +0800

    微服务改造 - 服务接口模块

---
+++ ai-code-microservice/ai-code-client/src/main/java/com/suoyike/aicodeclient/innerservice/InnerScreenshotService.java
@@ -0,0 +1,9 @@
+package com.suoyike.aicodeclient.innerservice;
+
+/**
+ * 内部截图服务
+ */
+public interface InnerScreenshotService {
+
+    String generateAndUploadScreenshot(String webUrl);
+}
---
+++ ai-code-microservice/ai-code-client/src/main/java/com/suoyike/aicodeclient/innerservice/InnerUserService.java
@@ -0,0 +1,35 @@
+package com.suoyike.aicodeclient.innerservice;
+
+import com.suoyike.aicodecommon.exception.BusinessException;
+import com.suoyike.aicodecommon.exception.ErrorCode;
+import com.suoyike.aicodemodel.model.entity.User;
+import com.suoyike.aicodemodel.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import static com.suoyike.aicodecommon.constant.UserConstant.USER_LOGIN_STATE;
+
+/**
+ * 内部使用的用户服务
+ */
+public interface InnerUserService {
+
+    List<User> listByIds(Collection<? extends Serializable> ids);
+
+    User getById(Serializable id);
+
+    UserVO getUserVO(User user);
+
+    // 静态方法，避免跨服务调用
+    static User getLoginUser(HttpServletRequest request) {
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        return currentUser;
+    }
+}
